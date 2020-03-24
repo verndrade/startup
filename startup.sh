d8f52646e5771451effbb8558a4af44eb7c102d1
Vernon Andrade <43098333+verndrade@users.noreply.github.com>
2020-03-23 20:49:21 -0500

    Add files via upload

---
+++ startup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+sudo apt update -y
+sudo apt upgrade -y     
+sudo apt full-upgrade -y
+sudo apt autoremove -y
+ln -s /mnt/c/users/vernon/desktop desktop
+if [ "$(tail -n 1 ~/.bashrc)" != "#" ]
+then
+    echo "alias desktop='cd && cd desktop'" >> ~/.bashrc
+    echo "alias daltz='cd && cd desktop/project-101-daltz'" >> ~/.bashrc
+    echo "alias capstone='cd && cd desktop/capstone'" >> ~/.bashrc
+    echo "alias runserver='python3 manage.py makemigrations && python3 manage.py migrate && python3 manage.py runserver'" >> ~/.bashrc
+    echo "alias runtest='python3 manage.py test'" >> ~/.bashrc
+    echo "alias python='python3'" >> ~/.bashrc
+    echo "alias pip='pip3'" >> ~/.bashrc
+    echo "alias bashrc='nano ~/.bashrc'" >> ~/.bashrc
+    echo "#" >> ~/.bashrc
+fi
