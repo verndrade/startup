6bb034dcba6acceda9b2779002393c05a13f90c5
Vernon Andrade <andradevern@gmail.com>
2020-04-15 15:26:33 -0500

    update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 sudo apt update -y
-sudo apt upgrade -y     
+sudo apt upgrade -y
 sudo apt full-upgrade -y
 sudo apt autoremove -y
 sudo apt install python3-pip -y
 ln -s /mnt/c/users/vernon/desktop desktop
+git config --global user.email "andradevern@gmail.com"
+git config --global user.name "Vernon Andrade"
 if [ "$(tail -n 1 ~/.bashrc)" != "#" ]
 then
     echo "alias desktop='cd && cd desktop'" >> ~/.bashrc
@@ -14,6 +16,7 @@ then
     echo "alias runserver='python3 manage.py makemigrations && python3 manage.py migrate && python3 manage.py runserver'" >> ~/.bashrc
     echo "alias runtest='python3 manage.py test'" >> ~/.bashrc
     echo "alias python='python3'" >> ~/.bashrc
+    echo "alias py='python3'" >> ~/.bashrc
     echo "alias pip='pip3'" >> ~/.bashrc
     echo "alias bashrc='nano ~/.bashrc'" >> ~/.bashrc
     echo "#" >> ~/.bashrc
