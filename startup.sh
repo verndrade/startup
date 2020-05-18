ad2864e59d8c63cdce9254700ba6019bb68dca1d
Vernon Andrade <43098333+verndrade@users.noreply.github.com>
2020-05-18 01:25:34 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -2,16 +2,14 @@
 sudo apt update -y
 sudo apt upgrade -y
 sudo apt full-upgrade -y
-sudo apt autoremove -y
 sudo apt install python3-pip -y
+sudo apt autoremove -y
 ln -s /mnt/c/users/vernon/desktop desktop
 git config --global user.email "andradevern@gmail.com"
 git config --global user.name "Vernon Andrade"
 if [ "$(tail -n 1 ~/.bashrc)" != "#" ]
 then
     echo "alias desktop='cd && cd desktop'" >> ~/.bashrc
-    echo "alias daltz='cd && cd desktop/project-101-daltz'" >> ~/.bashrc
-    echo "alias capstone='cd && cd desktop/capstone'" >> ~/.bashrc
     echo "alias untar='tar -xvf'" >> ~/.bashrc
     echo "alias runserver='python3 manage.py makemigrations && python3 manage.py migrate && python3 manage.py runserver'" >> ~/.bashrc
     echo "alias runtest='python3 manage.py test'" >> ~/.bashrc
